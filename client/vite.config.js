69611b403d5773540cca0f972fa01ebcc2ddbc8d
rebel600 <shyampareek232@gmail.com>
2026-04-30 11:29:13 +0530

    Fix:Vite env

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const API_URL = import.meta.env.VITE_API_URL_PRODUCTION || import.meta.env.VITE_API_URL_DEVELOPMENT;
+const API_URL = import.meta.env.VITE_API_URL
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
