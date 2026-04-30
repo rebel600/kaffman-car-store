20422ac5465bbfe1579909ccbe4063cd4e626566
rebel600 <shyampareek232@gmail.com>
2026-04-30 11:00:34 +0530

    Fix:production env for client

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+const API_URL = import.meta.env.VITE_API_URL_PRODUCTION || import.meta.env.VITE_API_URL_DEVELOPMENT;
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        // target: "http://localhost:5713",
-        target: "https://kaffman-car-store.onrender.com",
+        // target: "",
+        target: API_URL,
         changeOrigin: true,
       }
     },
