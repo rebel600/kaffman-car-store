56ae70fc6d52d0f47242d475f7a4e321734bc8a9
rebel600 <shyampareek232@gmail.com>
2026-04-30 12:17:01 +0530

    Fix: add PATCH method in cors.

--- server/middlewares/cors.middleware.js
+++ server/middlewares/cors.middleware.js
@@ -15,7 +15,7 @@ export const corsMiddleware = (req, res, next) => {
   }
 
   res.setHeader('Vary', 'Origin');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (req.method === 'OPTIONS') {
